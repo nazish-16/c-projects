ed87cba6d07bc050c4ddaee4ded68836510df37e
Frezz1ck <84974655+nazish-16@users.noreply.github.com>
2024-09-16 21:27:52 +0530

    switch cases

---
+++ cases.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    switch (a)
+    {
+    case 1:
+        printf("you entered 1\n");
+        break;
+    case 2:
+        printf("you entered 2\n");
+        break;
+    case 3:
+        printf("you entered 3\n");
+        break;
+    case 4:
+        printf("you entered 4\n");
+        break;
+    default:
+        break;
+    }
+}
